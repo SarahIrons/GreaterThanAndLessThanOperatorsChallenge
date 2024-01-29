0048d6e4338ab5755df02cf7af768b2d6e6b7969
SarahIrons <sarahirons11@gmail.com>
2024-01-29 09:36:13 -0500

    Add project files.

---
+++ GreaterThanAndLessThanOperatorsChallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GreaterThanAndLessThanOperatorsChallenge
+{
+    class Program
+    {
+       static void Main()
+        {
+            //Utilize the greater than operator and display output to the console.
+            int discountAge = 56;
+            if (discountAge > 60)
+            {
+                Console.WriteLine("Customer qualifies for a Senior Discount.");
+            }
+            else
+                Console.WriteLine("Customer does not qualify for a Senior Discount.");
+            {
+
+                //Utilize the lesser than or equal to operator and display output to the console.
+                int lifeguardAge = 15;
+                if (lifeguardAge <= 16)
+                {
+                    Console.WriteLine("You are not eligible to be a lifeguard at the pool.");
+                }
+                else
+                {
+                    Console.WriteLine("You should apply to be a lifeguard at the pool!");
+                }
+            }
+        }
+    }
+}
+    
+
